5ad067dd8b7739af5ce50cce92976e12798de971
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-12-01 00:44:12 +0100

    delete old plaid stuff

--- apps/api/src/routes/go-cardless/models.ts
+++
@@ -1,5 +0,0 @@
-import { Schema } from "effect"
-
-export class CreateLinkResponse extends Schema.Class<CreateLinkResponse>("CreateLinkResponse")({
-	link: Schema.String,
-}) {}
--- apps/api/src/routes/main/api.ts
+++
@@ -1,6 +0,0 @@
-import { HttpApiEndpoint, HttpApiGroup } from "@effect/platform"
-import { Schema } from "effect"
-
-export class BaseApi extends HttpApiGroup.make("api").add(
-	HttpApiEndpoint.get("healthCheck", "/").addSuccess(Schema.String),
-) {}
