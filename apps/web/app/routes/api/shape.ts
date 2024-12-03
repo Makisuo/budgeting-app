b8543cc4aa081a589e4f368d5b3ab621721e6b1a
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-12-03 20:32:04 +0100

    add base idk

---
+++ apps/web/app/routes/api/shape.ts
@@ -0,0 +1,45 @@
+import { getAuth } from '@clerk/tanstack-start/server'
+import { createAPIFileRoute } from '@tanstack/start/api'
+import { getWebRequest } from 'vinxi/http'
+
+const ELECTRIC_URL = import.meta.env.ELECTRIC_URL
+
+const publicTable = ['institutions']
+
+export const Route = createAPIFileRoute('/api/shape')({
+  GET: async ({ request }) => {
+    const { userId } = await getAuth(getWebRequest())
+
+    if (!userId) {
+      return new Response(null, { status: 401 })
+    }
+
+    const url = new URL(request.url)
+    const table = url.searchParams.get('table') as string
+
+    const originUrl = new URL('/v1/shape', ELECTRIC_URL)
+
+    if (!publicTable.includes(table)) {
+      originUrl.searchParams.set('where', `tenant_id = '${userId}'`)
+    }
+
+    url.searchParams.forEach((value, key) => {
+      if (['live', 'table', 'handle', 'offset', 'cursor'].includes(key)) {
+        originUrl.searchParams.set(key, value)
+      }
+    })
+
+    let resp = await fetch(originUrl.toString())
+    if (resp.headers.get('content-encoding')) {
+      const headers = new Headers(resp.headers)
+      headers.delete('content-encoding')
+      headers.delete('content-length')
+      resp = new Response(resp.body, {
+        status: resp.status,
+        statusText: resp.statusText,
+        headers,
+      })
+    }
+    return resp
+  },
+})
