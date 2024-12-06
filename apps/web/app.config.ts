58bd126931c6a2f1c63b481226d5075efb8cfff0
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-12-07 00:05:53 +0100

    stuff

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -1,13 +1,8 @@
-import { join } from "node:path"
 import { defineConfig } from "@tanstack/start/config"
-import type { App } from "vinxi"
 import tsConfigPaths from "vite-tsconfig-paths"
-import { getCloudflareProxyEnv, isInCloudflareCI } from "./app/lib/cloudflare"
 
-const app = defineConfig({
+export default defineConfig({
 	vite: {
-		define: await proxyCloudflareEnv(),
-
 		ssr: {
 			noExternal: ["@electric-sql/pglite-react", "react-scan"],
 		},
@@ -37,38 +32,6 @@ const app = defineConfig({
 		},
 	},
 	server: {
-		preset: "cloudflare-pages",
-		rollupConfig: {
-			external: ["node:async_hooks"],
-		},
+		preset: "vercel",
 	},
 })
-
-async function proxyCloudflareEnv() {
-	if (isInCloudflareCI()) return undefined
-
-	const env = await getCloudflareProxyEnv()
-
-	const viteDefine = Object.fromEntries(
-		Object.entries(env)
-			.filter(([key]) => key.startsWith("VITE_"))
-			.map(([key, value]) => [`import.meta.env.${key}`, `"${value}"`]),
-	)
-
-	return viteDefine
-}
-
-function withGlobalMiddleware(app: App) {
-	return {
-		...app,
-		config: {
-			...app.config,
-			routers: app.config.routers.map((router) => ({
-				...router,
-				middleware: router.target !== "server" ? undefined : join("app", "global-middleware.ts"),
-			})),
-		},
-	}
-}
-
-export default withGlobalMiddleware(app)
