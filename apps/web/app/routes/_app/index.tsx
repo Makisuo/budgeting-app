f72eda93609a4f7f620037a242048469fdf585a4
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-12-07 20:57:52 +0100

    add aggregated fields

--- apps/web/app/routes/_app/index.tsx
+++ apps/web/app/routes/_app/index.tsx
@@ -1,25 +1,77 @@
 import { createFileRoute } from "@tanstack/react-router"
+import type { Account } from "db"
+import { useMemo } from "react"
 import { Card } from "~/components/ui"
-
-import { Button } from "~/components/ui/button"
+import { useDrizzleLive } from "~/lib/hooks/use-drizzle-live"
+import { currencyFormatter } from "~/utils/formatters"
 
 export const Route = createFileRoute("/_app/")({
 	component: Home,
 })
 
+const getTotalAggregatedStats = (accounts: Account[]) => {
+	const totals = accounts.reduce((totals: { [currency: string]: number }, account) => {
+		console.log(account)
+		return {
+			// biome-ignore lint/performance/noAccumulatingSpread: <explanation>
+			...totals,
+			[account.balanceCurrency]: (totals[account.balanceCurrency] || 0) + account.balanceAmount,
+		}
+	}, {})
+
+	return {
+		balances: Object.entries(totals)
+			.map(([currency, balance]) => ({
+				currency,
+				balance,
+			}))
+			.sort((a, b) => b.balance - a.balance),
+	}
+}
+
 function Home() {
+	const { data } = useDrizzleLive((db) =>
+		db.query.accounts.findMany({
+			with: {
+				institution: true,
+			},
+		}),
+	)
+
+	const usageData = useMemo(() => {
+		return getTotalAggregatedStats(data)
+	}, [data])
+
+	console.log(usageData)
+
 	return (
 		<div>
-			<Card>
-				<Card.Header>
-					<Card.Title>Monthly Report</Card.Title>
-					<Card.Description>Financial summary for June</Card.Description>
-				</Card.Header>
-				<Card.Content>WOW</Card.Content>
-				<Card.Footer>
-					<Button>View Details</Button>
-				</Card.Footer>
-			</Card>
+			<div className="grid grid-cols-12 gap-3">
+				<Card className="col-span-4 space-y-1 p-4">
+					<div className="flex items-center gap-2">
+						<p className="text-muted-fg text-sm">Total Balance</p>
+					</div>
+					<div>
+						{usageData.balances.map((balance) => (
+							<p className="text-xl first:font-semibold first:text-3xl" key={balance.currency}>
+								{currencyFormatter(balance.currency).format(balance.balance)}
+							</p>
+						))}
+					</div>
+				</Card>
+				<Card className="col-span-4 space-y-1 p-4">
+					<div className="flex items-center gap-2">
+						<p className="text-muted-fg text-sm">Montlhy Expenses</p>
+					</div>
+					<h3 className="font-semibold text-3xl">1100$</h3>
+				</Card>
+				<Card className="col-span-4 space-y-1 p-4">
+					<div className="flex items-center gap-2">
+						<p className="text-muted-fg text-sm">Montlhy Earnings</p>
+					</div>
+					<h3 className="font-semibold text-3xl">1100$</h3>
+				</Card>
+			</div>
 		</div>
 	)
 }
