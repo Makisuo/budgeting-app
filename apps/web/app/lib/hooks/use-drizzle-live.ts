089c9a4d5089509cc0dcc8a388498c3c352dcb3c
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-12-12 22:48:20 +0100

    update pglite-drizzle

--- apps/web/app/lib/hooks/use-drizzle-live.ts
+++ apps/web/app/lib/hooks/use-drizzle-live.ts
@@ -1,4 +1,7 @@
 import { createDrizzle } from "@makisuo/pglite-drizzle/react"
 import { schema } from "db"
 
-export const { useDrizzleLive, useDrizzleLiveIncremental } = createDrizzle({ schema, casing: "camelCase" })
+export const { useDrizzleLive, useDrizzleLiveIncremental, syncShapeToTable } = createDrizzle({
+	schema,
+	casing: "camelCase",
+})
--- apps/web/app/utils/pglite/pglite.worker.ts
+++ apps/web/app/utils/pglite/pglite.worker.ts
@@ -1,10 +1,11 @@
 import { IdbFs, PGlite } from "@electric-sql/pglite"
-import { type SyncShapeToTableOptions, type SyncShapeToTableResult, electricSync } from "@electric-sql/pglite-sync"
+import { electricSync } from "@electric-sql/pglite-sync"
 import { live } from "@electric-sql/pglite/live"
 import { type PGliteWorkerOptions, worker } from "@electric-sql/pglite/worker"
 
 import type { schema } from "db"
 import type { ExtractTablesWithRelations } from "drizzle-orm"
+import { syncShapeToTable } from "~/lib/hooks/use-drizzle-live"
 import M1 from "./migrations.sql?raw"
 
 const ELECTRIC_URL = new URL("/api/electric/v1/shape", import.meta.env.VITE_BASE_URL).href
@@ -12,29 +13,6 @@ const ELECTRIC_URL = new URL("/api/electric/v1/shape", import.meta.env.VITE_BASE
 export const DB_NAME = "maple_db"
 
 export type ExtractedTables = ExtractTablesWithRelations<typeof schema>
-
-const syncTable = <
-	TTableKey extends keyof ExtractedTables,
-	TPrimaryKey extends keyof ExtractedTables[TTableKey]["columns"],
->(
-	pgLite: PGlite & {
-		electric: {
-			initMetadataTables: () => Promise<void>
-			syncShapeToTable: (options: SyncShapeToTableOptions) => Promise<SyncShapeToTableResult>
-		}
-	},
-	options: {
-		table: TTableKey
-		primaryKey: TPrimaryKey
-	} & Omit<SyncShapeToTableOptions, "table" | "primaryKey">,
-) => {
-	return pgLite.electric.syncShapeToTable({
-		shape: options.shape,
-		table: options.table,
-		primaryKey: [options.primaryKey as string],
-		shapeKey: options.shapeKey,
-	})
-}
 
 worker({
 	async init(options: PGliteWorkerOptions) {
@@ -50,42 +28,33 @@ worker({
 
 		await pg.exec(M1)
 
-		await syncTable(pg, {
+		await syncShapeToTable(pg, {
 			table: "institutions",
 			primaryKey: "id",
 			shapeKey: "institutions",
 			shape: {
 				url: ELECTRIC_URL,
 				onError: (error) => console.log(error),
-				params: {
-					table: "institutions",
-				},
 			},
 		})
 
-		await syncTable(pg, {
+		await syncShapeToTable(pg, {
 			table: "accounts",
 			primaryKey: "id",
 			shapeKey: "accounts",
 			shape: {
 				url: ELECTRIC_URL,
 				onError: (error) => console.log(error),
-				params: {
-					table: "accounts",
-				},
 			},
 		})
 
-		await syncTable(pg, {
+		await syncShapeToTable(pg, {
 			table: "transactions",
 			primaryKey: "id",
 			shapeKey: "transactions",
 			shape: {
 				url: ELECTRIC_URL,
 				onError: (error) => console.log(error),
-				params: {
-					table: "transactions",
-				},
 			},
 		})
 
