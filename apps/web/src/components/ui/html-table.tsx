e337892d195fe3782c579f6c0c6eede7f1d856de
Makisuo <31933546+Makisuo@users.noreply.github.com>
2025-01-21 23:21:40 +0100

    add html table

---
+++ apps/web/src/components/ui/html-table.tsx
@@ -0,0 +1,98 @@
+import type React from "react"
+
+import { tv } from "tailwind-variants"
+
+import { cn } from "~/utils/classes"
+
+const table = tv({
+	slots: {
+		root: "table w-full min-w-full caption-bottom border-spacing-0 text-sm outline-hidden [--table-selected-bg:color-mix(in_oklab,var(--color-primary)_5%,white_90%)] **:data-drop-target:border **:data-drop-target:border-primary dark:[--table-selected-bg:color-mix(in_oklab,var(--color-primary)_25%,black_70%)]",
+		header: "x32 border-b",
+		row: "tr group relative cursor-default border-b bg-bg text-fg/70 outline-hidden ring-primary data-selected:data-hovered:bg-(--table-selected-bg)/70 data-selected:bg-(--table-selected-bg) data-focus-visible:ring-1 data-focused:ring-0 dark:data-selected:data-hovered:bg-[color-mix(in_oklab,var(--color-primary)_40%,black_60%)] dark:data-selected:data-hovered:bg-subtle/60",
+		cellIcon:
+			"grid size-[1.15rem] flex-none shrink-0 place-content-center rounded bg-secondary text-fg *:data-[slot=icon]:size-3.5 *:data-[slot=icon]:shrink-0 *:data-[slot=icon]:transition-transform *:data-[slot=icon]:duration-200",
+	},
+})
+
+const { root, header, row, cellIcon } = table()
+
+interface TableProps extends React.HTMLProps<HTMLTableElement> {}
+
+const Table = ({ children, className, ...props }: TableProps) => (
+	<div className="relative w-full overflow-auto">
+		<table {...props} className={root({ className })}>
+			{children}
+		</table>
+	</div>
+)
+
+const Body = (props: React.HTMLProps<HTMLDivElement>) => (
+	<div {...props} className={cn("[&_.tr:last-child]:border-0")} />
+)
+
+const cellStyles = tv({
+	base: "group whitespace-nowrap px-3 py-3 outline-hidden",
+	variants: {
+		allowResize: {
+			true: "overflow-hidden truncate",
+		},
+	},
+})
+const TableCell = ({ children, className, ...props }: React.HTMLProps<HTMLTableCellElement>) => {
+	return (
+		<td {...props} className={cellStyles({ className })}>
+			{children}
+		</td>
+	)
+}
+
+const columnStyles = tv({
+	base: "relative allows-sorting:cursor-pointer whitespace-nowrap px-3 py-3 text-left font-medium outline-hidden data-dragging:cursor-grabbing [&:has([slot=selection])]:pr-0",
+	variants: {
+		isResizable: {
+			true: "overflow-hidden truncate",
+		},
+	},
+})
+
+const TableColumn = ({ className, ...props }: React.HTMLProps<HTMLTableCellElement>) => {
+	return (
+		<th
+			data-slot="table-column"
+			{...props}
+			className={columnStyles({
+				className,
+			})}
+		/>
+	)
+}
+
+const Header = ({ className, ...props }: React.HTMLProps<HTMLTableRowElement>) => {
+	return <tr className={header({ className })} {...props} />
+}
+
+const TableRow = ({ children, className, id, ref, ...props }: React.HTMLProps<HTMLTableRowElement>) => {
+	return (
+		<tr
+			ref={ref}
+			{...props}
+			className={row({
+				className:
+					"href" in props
+						? cn("cursor-pointer data-hovered:bg-secondary/50 data-hovered:text-secondary-fg", className)
+						: "",
+			})}
+		>
+			{children}
+		</tr>
+	)
+}
+
+Table.Body = Body
+Table.Cell = TableCell
+Table.Column = TableColumn
+Table.Header = Header
+Table.Row = TableRow
+
+export type { TableProps }
+export { Table }
--- apps/web/src/routes/_app/accounts/-components/transaction-table.tsx
+++ apps/web/src/routes/_app/accounts/-components/transaction-table.tsx
@@ -1,6 +1,7 @@
 import { IconCirclePlaceholderDashed } from "justd-icons"
 import { DateValue } from "~/components/date-value"
-import { Badge, Table } from "~/components/ui"
+import { Badge } from "~/components/ui"
+import { Table } from "~/components/ui/html-table"
 import { currencyFormatter } from "~/utils/formatters"
 import { useDrizzleLive } from "~/utils/pglite/drizzle-client"
 
@@ -20,47 +21,45 @@ export const TransactionTable = ({ accountId }: { accountId: string }) => {
 	return (
 		<Table aria-label="Daemons">
 			<Table.Header>
-				<Table.Column isRowHeader>Name</Table.Column>
+				<Table.Column>Name</Table.Column>
 				<Table.Column>Price</Table.Column>
 				<Table.Column>Category</Table.Column>
 				<Table.Column>Date</Table.Column>
 				<Table.Column>Status</Table.Column>
 			</Table.Header>
-			<Table.Body items={transactions}>
-				{(transaction) => (
-					<Table.Row key={transaction.id}>
-						<Table.Cell className="flex items-center gap-2">
-							{transaction.company ? (
-								<img
-									className="size-6 rounded-md"
-									src={`https://cdn.brandfetch.io/${transaction.company.url}/w/512/h/512?c=1id0IQ-4i8Z46-n-DfQ`}
-									alt={transaction.company.name}
-								/>
-							) : (
-								<IconCirclePlaceholderDashed className="size-6" />
-							)}
+			{transactions.map((transaction) => (
+				<Table.Row key={transaction.id}>
+					<Table.Cell className="flex items-center gap-2">
+						{transaction.company ? (
+							<img
+								className="size-6 rounded-md"
+								src={`https://cdn.brandfetch.io/${transaction.company.url}/w/512/h/512?c=1id0IQ-4i8Z46-n-DfQ`}
+								alt={transaction.company.name}
+							/>
+						) : (
+							<IconCirclePlaceholderDashed className="size-6" />
+						)}
 
-							{transaction.company?.name || transaction.name}
-						</Table.Cell>
-						<Table.Cell>
-							<Badge intent={Number(transaction.amount) < 0 ? "danger" : "success"}>
-								{currencyFormatter(transaction.currency ?? "USD").format(transaction.amount)}
-							</Badge>
-						</Table.Cell>
-						<Table.Cell>
-							<Badge>{transaction.category.name}</Badge>
-						</Table.Cell>
-						<Table.Cell>
-							<DateValue date={transaction.date} />
-						</Table.Cell>
-						<Table.Cell>
-							<Badge intent={transaction.status === "pending" ? "info" : "primary"}>
-								{transaction.status === "pending" ? "Pending" : "Completed"}
-							</Badge>
-						</Table.Cell>
-					</Table.Row>
-				)}
-			</Table.Body>
+						{transaction.company?.name || transaction.name}
+					</Table.Cell>
+					<Table.Cell>
+						<Badge intent={Number(transaction.amount) < 0 ? "danger" : "success"}>
+							{currencyFormatter(transaction.currency ?? "USD").format(transaction.amount)}
+						</Badge>
+					</Table.Cell>
+					<Table.Cell>
+						<Badge>{transaction.category.name}</Badge>
+					</Table.Cell>
+					<Table.Cell>
+						<DateValue date={transaction.date} />
+					</Table.Cell>
+					<Table.Cell>
+						<Badge intent={transaction.status === "pending" ? "info" : "primary"}>
+							{transaction.status === "pending" ? "Pending" : "Completed"}
+						</Badge>
+					</Table.Cell>
+				</Table.Row>
+			))}
 		</Table>
 	)
 }
