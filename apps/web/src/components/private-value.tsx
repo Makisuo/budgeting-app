64569060634c0d1375432f2f71842681e6193d67
Makisuo <31933546+Makisuo@users.noreply.github.com>
2025-01-27 00:12:13 +0100

    fix blur

--- apps/web/src/components/private-value.tsx
+++ apps/web/src/components/private-value.tsx
@@ -3,5 +3,5 @@ import { usePrivacyMode } from "~/lib/global-store"
 export const PrivateValue = ({ children }: { children: React.ReactNode }) => {
 	const [isPrivate] = usePrivacyMode()
 
-	return <span className={isPrivate ? "blur-[3px] filter" : ""}>{children}</span>
+	return <span className={isPrivate ? "blur-sm filter" : ""}>{children}</span>
 }
