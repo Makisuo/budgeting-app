5526036b8fd30f6be091dc6563fbbb8045469da9
Makisuo <31933546+Makisuo@users.noreply.github.com>
2025-01-26 23:58:03 +0100

    stuff

--- apps/web/src/components/private-value.tsx
+++ apps/web/src/components/private-value.tsx
@@ -2,5 +2,6 @@ import { usePrivacyMode } from "~/lib/global-store"
 
 export const PrivateValue = ({ children }: { children: React.ReactNode }) => {
 	const [isPrivate] = usePrivacyMode()
+
 	return <span className={isPrivate ? "blur-[3px] filter" : ""}>{children}</span>
 }
